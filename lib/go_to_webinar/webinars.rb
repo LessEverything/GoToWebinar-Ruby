1d7d01c6253e74d650b97768aa93548689e24cdf
Chet Bortz <chet@bortz.co>
2016-07-07 01:28:44 -0400

    Add wrapper for creating webinars

--- lib/go_to_webinar/webinars.rb
+++ lib/go_to_webinar/webinars.rb
@@ -1,25 +1,27 @@
 module GoToWebinar
   module Webinars
-    
     def get_historical_webinars(params = {})
       self.class.get("historicalWebinars", :query => params)
     end
-    
+
     def get_upcoming_webinars(params = {})
       self.class.get("upcomingWebinars", :query => params)
     end
-    
+
     def get_webinar(webinar_key)
       self.class.get("webinars/#{webinar_key}")
     end
-    
+
     def get_webinar_meeting_times(webinar_key)
       self.class.get("webinars/#{webinar_key}/meetingTimes")
     end
-    
+
     def get_webinars()
       self.class.get("webinars")
     end
-    
+
+    def create_webinar(params = {})
+      self.class.post("webinars", :body => params.to_json)
+    end
   end
 end
--- test/test_webinars.rb
+++ test/test_webinars.rb
@@ -15,7 +15,7 @@ class WebinarsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["webinarKey"]
     end
-    
+
     should "generate valid upcoming webinars" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/54321/upcomingWebinars",:body => '[{"webinarKey":"54321"}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_upcoming_webinars()
@@ -24,7 +24,7 @@ class WebinarsTest < Test::Unit::TestCase
       assert_not_nil @r.parsed_response.first["webinarKey"]
 
     end
-    
+
     should "generate valid get webinar" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/54321/webinars/54321",:body => '[{"webinarKey":"54321"}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_webinar("54321")
@@ -40,7 +40,7 @@ class WebinarsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["startTime"]
     end
-    
+
     should "generate valid get webinars" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/54321/webinars",:body => '[{"webinarKey":"54321"}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_webinars()
@@ -48,6 +48,25 @@ class WebinarsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["webinarKey"]
     end
-    
+
+    should "create a webinar with valid params" do
+      FakeWeb.register_uri(:post, "https://api.citrixonline.com/G2W/rest/organizers/54321/webinars",:body => '{"webinarKey":"54321"}', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.create_webinar(creation_params)
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Hash)
+      assert_not_nil @r.parsed_response["webinarKey"]
+    end
+  end
+
+  def creation_params
+    {
+      "subject":              "string",
+      "description":          "string",
+      "timeZone":             "America/New_York",
+      "type":                 "single_session",
+      "isPasswordProtected":  false,
+      "times":                [{  "startTime":  (Time.now + 3600).iso8601,
+                                  "endTime":    (Time.now + 7200).iso8601   }]
+    }
   end
 end
