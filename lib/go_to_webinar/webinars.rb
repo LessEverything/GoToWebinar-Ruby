72244f05bcb555c2090220c34c686b1938c9fe5b
Chet Bortz <chet@bortz.co>
2016-08-24 15:43:40 -0400

    Fix: Use correct HTTParty syntax
    calls to `#post`, `#get`, etc. should always prefix a forward slash as
    noted in their README: https://github.com/jnunemaker/httparty#examples
    
    Prior to HTTParty version 0.14.x, we didn’t encounter a problem.
    However, after version 0.14.x was released, URLs will not respect a
    trailing slash in the base URI.

--- lib/go_to_webinar/webinars.rb
+++ lib/go_to_webinar/webinars.rb
@@ -1,33 +1,33 @@
 module GoToWebinar
   module Webinars
     def get_historical_webinars(params = {})
-      self.class.get("historicalWebinars", :query => params)
+      self.class.get("/historicalWebinars", :query => params)
     end
 
     def get_upcoming_webinars(params = {})
-      self.class.get("upcomingWebinars", :query => params)
+      self.class.get("/upcomingWebinars", :query => params)
     end
 
     def get_webinar(webinar_key)
-      self.class.get("webinars/#{webinar_key}")
+      self.class.get("/webinars/#{webinar_key}")
     end
 
     def get_webinar_meeting_times(webinar_key)
-      self.class.get("webinars/#{webinar_key}/meetingTimes")
+      self.class.get("/webinars/#{webinar_key}/meetingTimes")
     end
 
     def get_webinars()
-      self.class.get("webinars")
+      self.class.get("/webinars")
     end
 
     def create_webinar(params = {})
-      self.class.post("webinars", :body => params.to_json)
+      self.class.post("/webinars", :body => params.to_json)
     end
 
     # TODO: Add tests
     def update_webinar(webinar_key, params = {})
       # TODO: Add support for 'notifyParticipants' (https://developer.citrixonline.com/content/gotowebinar-api-reference#!/Webinars/updateWebinar)
-      self.class.put("webinars/#{webinar_key}", :body => params.to_json)
+      self.class.put("/webinars/#{webinar_key}", :body => params.to_json)
     end
   end
 end
